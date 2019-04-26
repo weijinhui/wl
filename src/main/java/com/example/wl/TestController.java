b7074359adbf7a6fe32144c070bb31d5c510ccdf
weijinhui <413296121@qq.com>
2019-04-26 17:48:49 +0800

    asd

--- src/main/java/com/example/wl/TestController.java
+++ src/main/java/com/example/wl/TestController.java
@@ -5,22 +5,60 @@ package com.example.wl;/**
  */
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @program: wl
- *
  * @description:
- *
  * @author: Carson Wei
- *
  * @create: 2019-04-26 16:02
  **/
 @Controller
 public class TestController {
 
     @RequestMapping("/test")
-    public String test(){
-      return "test";
+    public String test() {
+        return "test";
+    }
+
+    @PostMapping(value = "/testLogin")
+    @ResponseBody
+    public Map<String, String> testLogin(HttpServletRequest request, HttpServletResponse response,String urlstr,int number) {
+
+        Map<String, String> map = new HashMap<>();
+        try {
+            for (int i = 0; i < number; i++) {
+
+                URL url = new URL(urlstr);
+                InputStream in = url.openStream();
+                InputStreamReader isr = new InputStreamReader(in);
+                BufferedReader bufr = new BufferedReader(isr);
+                String str;
+                while ((str = bufr.readLine()) != null) {
+                    System.out.println(str);
+                }
+                bufr.close();
+                isr.close();
+                in.close();
+            }
+            map.put("code", "200");
+            map.put("message", "success");
+        } catch (Exception e) {
+            e.printStackTrace();
+            map.put("code", "500");
+            map.put("message", "异常");
+        }
+        return map;
     }
 }
